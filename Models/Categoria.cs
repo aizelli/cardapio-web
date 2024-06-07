bb2f26671e499b84639f93d906969f1ed95de360
aizelli <alexandre.izelli@gmail.com>
2024-06-07 10:34:23 -0300

    criando models categoria e produto

---
+++ Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
