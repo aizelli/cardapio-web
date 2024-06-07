9e9b1ac7818c8a5e2911a9a80734f8e966f6bdbb
vitaocodigo <victor.mishima@etce.sp.gov.br>
2024-06-07 10:43:14 -0300

    adc protudos

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -2,5 +2,14 @@
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao_Curta { get; set; }
+        public string Descricao_Detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string Local_Images { get; set; }
+        public string Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
+                
     }
 }
