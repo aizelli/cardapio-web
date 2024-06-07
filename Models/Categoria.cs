bf4daf0232fd88ab0e086b987540a8c629cbbfce
aizelli <alexandre.izelli@gmail.com>
2024-06-07 10:16:06 -0300

    criando migrations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,10 +1,23 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         //Propriedades (colunas) do objeto (tabela) Categorias 
+        [Key]
         public int Id { get; set; }
+
+        [Display(Name ="Nome da categoria")]
+        [Required(ErrorMessage ="Compo obrigatório")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo 50 caracteres")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição da categoria")]
+        [Required(ErrorMessage = "Compo obrigatório")]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "O {0} deve ter no mínimo {1} e no máximo 200 caracteres")]
         public string Descricao { get; set; }
 
         public List<Produto> Produtos { get; set; } 
--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,20 +1,48 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
         //Propriedades (colunas) do objeto (tabela) Produtos 
+        [Key]
         public int Id { get; set; }
+        
+        [Display(Name = "Nome do produto")]
+        [Required(ErrorMessage = "Compo obrigatório")]
+        [MinLength(10, ErrorMessage = "Nome do produto deve ter no mínimo {1} Caracteres")]
+        [MaxLength(100, ErrorMessage = "Nome do produto deve ter no máximo {1} Caracteres")]
         public string Nome { get; set; }
+
+        [Display(Name = "Descrição curta do produto")]
+        [Required(ErrorMessage = "Compo obrigatório")]
+        [MinLength(10, ErrorMessage = "A Descrição curta do produto deve ter no mínimo {1} Caracteres")]
+        [MaxLength(150, ErrorMessage = "A Descrição curta do produto deve ter no máximo {1} Caracteres")]
         public string Descricao_Curta { get; set; }
+
+        [Display(Name = "Descrição detalhada do produto")]
+        [Required(ErrorMessage = "Compo obrigatório")]
+        [MinLength(10, ErrorMessage = "A Descrição detalhada do produto deve ter no mínimo {1} Caracteres")]
+        [MaxLength(500, ErrorMessage = "A Descrição detalhada do produto deve ter no máximo {1} Caracteres")]
         public string Descricao_Detalhada { get; set; }
+
+        [Display(Name = "Preço do produto")]
+        [Required(ErrorMessage = "Compo obrigatório")]
+        [Column(TypeName ="decimal(10,2)")]
         public decimal Preco { get; set; }
+
+        [Display(Name = "Endereço da imagem do produto")]
         public string Local_Imagem { get; set; }
-        public string Minitura_Imagem { get; set; }
+
+        [Display(Name = "Produto preferido?")]
         public bool Produto_Preferido { get; set; }
+
+        [Display(Name = "Estoque do produto")]
         public bool Em_Estoque { get; set; }
 
         //Relacionamento com tabela Categoria
-        public int Categoria_Id { get; set; }
         public virtual Categoria Categoria { get; set; }
     }
 }
