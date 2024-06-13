467548a400a8095658979853c49c2ec661aa1ee8
aizelli <alexandre.izelli@gmail.com>
2024-06-13 10:42:02 -0300

    Alteração da Model Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,26 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Campo Obrigatório")]
+        [Display(Name ="Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Descricao_Curta { get; set; }
+
+        public string Descricao_Detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public bool Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
     }
 }
