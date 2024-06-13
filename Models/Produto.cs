0bad29c9da2d4ea9ab07205a2ca6ccdf4ae88948
Inácio <inacio.souto@etec.sp.gov.br>
2024-06-13 10:42:55 -0300

    Alteração Model Produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -1,6 +1,30 @@
-﻿namespace CardapioWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CardapioWeb.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Campo Obrigatório")]
+        [Display(Name = "Nome do Produto")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string NomeDescricao { get;}
+        public string Desceicao_Detelhada{ get; set; }  
+        public decimal Preco { get; set; }
+        public bool Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+
+
+
+        
+            
     }
 }
