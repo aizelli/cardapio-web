317e1315f89fdb5d104d4548293e52fee8eed4dc
aizelli <alexandre.izelli@gmail.com>
2024-06-14 09:15:14 -0300

    alteração produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -2,5 +2,13 @@
 {
     public class Produto
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao_Curta { get; set; }
+        public string Descricao_Detalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string Local_Imagem { get; set; }
+        public bool Produto_Preferido { get; set; }
+        public bool Em_Estoque { get; set; }
     }
 }
