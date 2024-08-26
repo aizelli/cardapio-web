a9ae11dc9245e89f7324cbb607fb04673e7fe48b
aizelli <alexandre.izelli@gmail.com>
2024-08-26 09:00:58 -0300

    adicionado funções crud para categorias

--- Repositories/CategoriaRepository.cs
+++ Repositories/CategoriaRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CardapioWeb.Context;
 using CardapioWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CardapioWeb.Repositories
 {
@@ -12,9 +13,54 @@ namespace CardapioWeb.Repositories
             _dbContext = dbContext;
         }
 
-        public IEnumerable<Categoria> GetAll()
+        public async Task Add(Categoria categoria)
         {
-            return _dbContext.Categorias.ToList();
+            try
+            {
+                _dbContext.Categorias.Add(categoria);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+
+        }
+
+        public async Task Delete(Categoria categoria)
+        {
+            try
+            {
+                _dbContext.Categorias.Remove(categoria);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+        }
+
+        public async Task<Categoria> GetById(int id)
+        {
+            return await _dbContext.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task Update(Categoria categoria)
+        {
+            try
+            {
+                _dbContext.Categorias.Update(categoria);
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+        }
+
+        public async Task<List<Categoria>> GetAll()
+        {
+            return await _dbContext.Categorias.ToListAsync();
         }
     }
 }
