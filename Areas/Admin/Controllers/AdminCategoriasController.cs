e0402e9d3ea560c3fd0690e995ceb62c077704d0
aizelli <alexandre.izelli@gmail.com>
2024-09-02 08:57:07 -0300

    configurando controller de categorias

--- Areas/Admin/Controllers/AdminCategoriasController.cs
+++ Areas/Admin/Controllers/AdminCategoriasController.cs
@@ -7,38 +7,33 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CardapioWeb.Context;
 using CardapioWeb.Models;
+using CardapioWeb.Repositories;
 
 namespace CardapioWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminCategoriasController : Controller
     {
-        private readonly AppDBContext _context;
+        private readonly ICategoriaRepository _categoriaRepository;
 
-        public AdminCategoriasController(AppDBContext context)
+        public AdminCategoriasController(ICategoriaRepository categoriaRepository)
         {
-            _context = context;
+            _categoriaRepository = categoriaRepository;
         }
 
         // GET: Admin/AdminCategorias
         public async Task<IActionResult> Index()
         {
-              return _context.Categorias != null ? 
-                          View(await _context.Categorias.ToListAsync()) :
-                          Problem("Entity set 'AppDBContext.Categorias'  is null.");
+              return _categoriaRepository.GetAll() != null ? 
+                          View(await _categoriaRepository.GetAll()) :
+                          Problem("Nenhum registro encontrado");
         }
 
         // GET: Admin/AdminCategorias/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Detalhes(int id)
         {
-            if (id == null || _context.Categorias == null)
-            {
-                return NotFound();
-            }
-
-            var categoria = await _context.Categorias
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoria == null)
+            var categoria = await _categoriaRepository.GetById(id);
+            if(categoria == null)
             {
                 return NotFound();
             }
@@ -47,7 +42,7 @@ namespace CardapioWeb.Areas.Admin.Controllers
         }
 
         // GET: Admin/AdminCategorias/Create
-        public IActionResult Create()
+        public IActionResult Cadastro()
         {
             return View();
         }
@@ -57,30 +52,24 @@ namespace CardapioWeb.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao")] Categoria categoria)
+        public async Task<IActionResult> Cadastro([Bind("Id,Nome,Descricao")] Categoria categoria)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(categoria);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+            if (categoria != null){
+                await _categoriaRepository.Add(categoria);
+                return RedirectToAction("Index");
             }
             return View(categoria);
         }
 
         // GET: Admin/AdminCategorias/Edit/5
-        public async Task<IActionResult> Edit(int? id)
+        public async Task<IActionResult> Edicao(int id)
         {
-            if (id == null || _context.Categorias == null)
+            var categoria = await _categoriaRepository.GetById(id);
+            if(categoria == null)
             {
                 return NotFound();
             }
 
-            var categoria = await _context.Categorias.FindAsync(id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
             return View(categoria);
         }
 
@@ -89,47 +78,28 @@ namespace CardapioWeb.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao")] Categoria categoria)
+        public async Task<IActionResult> Edicao(int id, [Bind("Id,Nome,Descricao")] Categoria categoria)
         {
             if (id != categoria.Id)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if(categoria != null)
             {
-                try
-                {
-                    _context.Update(categoria);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoriaExists(categoria.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                await _categoriaRepository.Update(categoria);
+                return RedirectToAction("Index");
             }
+
             return View(categoria);
+
         }
 
         // GET: Admin/AdminCategorias/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        public async Task<IActionResult> Delete(int id)
         {
-            if (id == null || _context.Categorias == null)
-            {
-                return NotFound();
-            }
-
-            var categoria = await _context.Categorias
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoria == null)
+            var categoria = await _categoriaRepository.GetById(id);
+            if(categoria == null)
             {
                 return NotFound();
             }
@@ -142,23 +112,13 @@ namespace CardapioWeb.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.Categorias == null)
+            var categoria = await _categoriaRepository.GetById(id);
+            if(categoria != null)
             {
-                return Problem("Entity set 'AppDBContext.Categorias'  is null.");
+                await _categoriaRepository.Delete(categoria);
             }
-            var categoria = await _context.Categorias.FindAsync(id);
-            if (categoria != null)
-            {
-                _context.Categorias.Remove(categoria);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
-        private bool CategoriaExists(int id)
-        {
-          return (_context.Categorias?.Any(e => e.Id == id)).GetValueOrDefault();
+            return View(categoria);
         }
     }
 }
